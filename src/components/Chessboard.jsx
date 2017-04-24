8222da8a6620d617ace8ba9b8098953c303999ea
brianarpie <brian.arpie@gmail.com>
2017-04-23 20:59:24 -0400

    add chess pieces and correct css layout with vmin, vh, vw helpers

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -3,17 +3,41 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import '../styles/pieces.css';
+import '../styles/chessboard.css';
+
+// const ROWS = ['a','b','c','d','e','f','g','h'];
+const ROWS = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
+const COLS = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
 
 class Chessboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pieces: []
+        }
+    }
     render() {
         return (
-            <div>
-                <div className="Piece White King"/>
-                <div className="Piece Black King"/>
-                <div className="Piece White Knight"/>
-                <div className="Piece Black Queen"/>
-                <div className="Piece Black Pawn"/>
-                <div className="Piece White Bishop"/>
+            <div className="Chessboard">
+                {ROWS.map(rowIndex => {
+                    return (
+                        <div className="Row"
+                            key={rowIndex}>
+                            {
+                                COLS.map(colIndex => {
+                                    return (
+                                        <div className={
+                                            "Square" + (rowIndex % 2 == 0 && colIndex % 2 == 0 || rowIndex % 2 == 1 && colIndex % 2 == 1 ? " White" : " Black")
+                                        }
+                                            key={`${rowIndex}${colIndex}`} >
+                                            <div className="Black Pawn Piece" />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    )
+                })}
             </div>
         )
     }
